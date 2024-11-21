b03625a23b4fc930112b3260e22bf4bff91cf03e
ameyakore <kore.ameya@gmail.com>
2024-11-21 20:54:32 +0530

    updated hash for arm

--- Formula/ocp.rb
+++ Formula/ocp.rb
@@ -4,16 +4,13 @@ class Ocp < Formula
     on_macos do
         on_arm do 
             url "https://github.com/fa993/output-copy/releases/download/0.1.0/ocp-0.1.0-macos-arm.tar.gz"
-            # sha256 "e17405adc655824dec3ca6e2a9a4b199a715743ed5f0948df58f6bb369267aa3"
+            sha256 "5afbd9526e3685c80c431f20dc91a81089f5fbfe6871379f47bc2644c684e8d7"
         end
         on_intel do
             url "https://github.com/fa993/output-copy/releases/download/0.1.0/ocp-0.1.0-macos-x86_64.tar.gz"
         end
     end
 
-    # url "https://github.com/fa993/output-copy/0.1.0.tarball.gz"
-    
-    version "0.1.0" 
     def install
       bin.install "ocp"
     end
